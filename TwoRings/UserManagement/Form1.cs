c02dc35d3ce6ef3f4bf035ee5399f0ddecda61a1
WINGEN\Austin <rivers.mka@gmail.com>
2017-08-20 19:10:33 -0700

    Created UserManagement Project

---
+++ TwoRings/UserManagement/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadMe();
+        }
+
+        public void LoadMe()
+        {
+            this.BackColor = Color.Red;
+        }
+    }
+}
