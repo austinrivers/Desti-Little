f8db8ed2f861e04adabee34aee0abbf63bdefa9f
WINGEN\Austin <rivers.mka@gmail.com>
2017-08-22 16:12:39 -0700

    Added AirlineManager with Plane and Route

---
+++ TwoRings/RouteConfigurator/Plane.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteConfigurator
+{
+
+    public enum RangeClass
+    {
+        Unknown,
+        ShortHaul,
+        MediumHaul,
+        LongHaul, 
+        Cargo
+    }
+
+    class Plane
+    {
+        public RangeClass rangeClass;
+        public string modelName;
+        public double Price;
+        public double consumption;
+        public double speed;
+        public double wareCoeficientPercent;
+        public int range;
+        public int categoryRequired;
+        public int maxEconomy;
+        public int maxBusiness;
+        public int maxFirst;
+        public int maxCargo;
+    }
+}
---
+++ TwoRings/RouteConfigurator/Route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteConfigurator
+{
+    public class Route
+    {
+        public string callsign;
+        public string airportName;
+        public int distance;
+        public int categoryMax;
+        public int taxPerTakeoff;
+        public int price;
+        public int demandEconomy;
+        public int demandBusiness;
+        public int demandFirst;
+        public int demandCargo;
+    }
+}
